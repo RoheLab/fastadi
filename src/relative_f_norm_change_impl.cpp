f80aa643162ce9c4b44545364dfff9c2536c2d79
YunyiShen <yshen99@wisc.edu>
2020-12-08 12:35:26 -0600

    remove last omp

--- src/relative_f_norm_change_impl.cpp
+++ src/relative_f_norm_change_impl.cpp
@@ -1,46 +1,78 @@
 #include <RcppArmadillo.h>
-#include <omp.h>
-// [[Rcpp::plugins(openmp)]]
-
+#include <RcppParallel.h>
+using namespace RcppParallel;
 using namespace arma;
+using namespace std;
 
 // [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::export]]
-double relative_f_norm_change_impl(
-    const arma::mat& new_U,
-    const arma::rowvec& new_d,
-    const arma::mat& new_V,
-    const arma::mat& U,
-    const arma::rowvec& d,
-    const arma::mat& V,
-    const int num_threads) {
+// [[Rcpp::depends(RcppParallel)]]
 
-  omp_set_num_threads(num_threads);
+struct diff_norm_sq_worker : public Worker
+{
+  const arma::mat &U;
+  const arma::mat &DVt;
+  const arma::mat &new_U;
+  const arma::mat &new_DVt;
+  double diff_norm_sq;
+
+  diff_norm_sq_worker(const arma::mat &U,
+                      const arma::mat &DVt,
+                      const arma::mat &new_U,
+                      const arma::mat &new_DVt) : U(U), DVt(DVt), new_U(new_U), new_DVt(new_DVt), diff_norm_sq(0) {}
+
+  diff_norm_sq_worker(const diff_norm_sq_worker &theworker,
+                      Split) : U(theworker.U), DVt(theworker.DVt), new_U(theworker.new_U), new_DVt(theworker.new_DVt), diff_norm_sq(0) {}
+
+  void operator()(std::size_t begin, std::size_t end)
+  {
+    for (int i = begin; i < end; i++)
+    {
+
+      arma::rowvec new_Z_i, Z_i, diff;
+
+      new_Z_i = new_U.row(i) * new_DVt;
+      Z_i = U.row(i) * DVt;
+
+      diff = new_Z_i - Z_i;
+      diff_norm_sq += dot(diff, diff);
+    }
+  }
+
+  void join(const diff_norm_sq_worker &theworker)
+  {
+    diff_norm_sq += theworker.diff_norm_sq;
+  }
+
+};
+
+
+
+// [[Rcpp::export]]
+double relative_f_norm_change_impl_cpp(
+    const arma::mat &new_U,
+    const arma::rowvec &new_d,
+    const arma::mat &new_V,
+    const arma::mat &U,
+    const arma::rowvec &d,
+    const arma::mat &V)
+{
+
 
   arma::mat new_DVt = diagmat(new_d) * new_V.t();
   arma::mat DVt = diagmat(d) * V.t();
-  
-  double diff_norm_sq = 0;
 
-  // expand a single row of new_Z and Z at a time
-  // to avoid hitting memory limits
-  
-  // http://jakascorner.com/blog/2016/06/omp-data-sharing-attributes.html
-  #pragma omp parallel for reduction(+: diff_norm_sq)
-  for (int i = 0; i < U.n_rows; i++) {
 
-    arma::rowvec new_Z_i, Z_i, diff;
+// expand a single row of new_Z and Z at a time
+// to avoid hitting memory limits
 
-    new_Z_i = new_U.row(i) * new_DVt;
-    Z_i = U.row(i) * DVt;
-
-    diff = new_Z_i - Z_i;
-    diff_norm_sq += dot(diff, diff);
-  }
+  diff_norm_sq_worker diff_norm(U,DVt,new_U,new_DVt);
+  parallelReduce(0,U.n_rows, diff_norm);
 
   // exploit connection between frobenius norm
   // and singular values
   double z_norm_sq = accu(pow(d, 2));
 
-  return diff_norm_sq / z_norm_sq;
+  return (diff_norm.diff_norm_sq / z_norm_sq);
 }
+
+
