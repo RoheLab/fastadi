bf0dd5b3edaf6bbbfa676c37a4e24ef5249eac78
Alex Hayes <alexpghayes@gmail.com>
2024-04-12 13:27:13 -0500

    Misc for Rcpp

--- src/fastadi_types.h
+++ src/fastadi_types.h
@@ -1,7 +1,7 @@
 #ifndef _FASTADI_TYPES
 #define _FASTADI_TYPES
 
-// #include "SparseMatrix.h"
+#include "SparseMatrix.h"
 
 // see section 2.5 of https://cran.r-project.org/web//packages/Rcpp/vignettes/Rcpp-attributes.pdf
 // and also https://stackoverflow.com/questions/69831212/non-intrusive-extension-of-rcpp-with-rcpptraitsexporter
