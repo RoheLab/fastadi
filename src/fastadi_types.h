c5759680691e75bfedf94a036008670dd8142fd4
Alex Hayes <alexpghayes@gmail.com>
2024-04-11 21:41:56 -0500

    Use Rcpp::XPtr to avoid some data copying
    - Yields 2x speedup

---
+++ src/fastadi_types.h
@@ -0,0 +1,14 @@
+#ifndef _FASTADI_TYPES
+#define _FASTADI_TYPES
+
+// #include "SparseMatrix.h"
+
+// see section 2.5 of https://cran.r-project.org/web//packages/Rcpp/vignettes/Rcpp-attributes.pdf
+// and also https://stackoverflow.com/questions/69831212/non-intrusive-extension-of-rcpp-with-rcpptraitsexporter
+
+// we have to forward declare any classes wrapped in Rcpp::XPtr that should
+// up as parts of type signatures. without this, sourceCpp() will work
+// but the package won't compile correctly
+class CitationEstimate;
+
+#endif
--- src/scratch.cpp
+++ src/scratch.cpp
@@ -1,111 +1,223 @@
 #include <RcppArmadillo.h>
 
+// [[Rcpp::depends(RcppArmadillo)]]
 
-class LRMatrix {
+class CitationEstimate {
   public:
     arma::mat U, V;
     arma::rowvec d;
-    LRMatrix(arma::mat, arma::rowvec, arma::mat);
+    arma::sp_mat A;
+    CitationEstimate(arma::sp_mat, arma::mat, arma::rowvec, arma::mat);
     arma::vec left_multiply(arma::vec);
+    arma::vec right_multiply(arma::vec);
+    double p_omega_z();
 };
 
-// constructor
-LRMatrix::LRMatrix(arma::mat U, arma::rowvec d, arma::mat V) {
-  U = U;
-  d = d;
-  V = V;
+CitationEstimate::CitationEstimate(arma::sp_mat citations, arma::mat U1, arma::rowvec d1, arma::mat V1) {
+  U = U1;
+  d = d1;
+  V = V1;
+  A = citations;
 }
 
-// method
-arma::vec LRMatrix::left_multiply(arma::vec x) {
-  return x;
+arma::vec CitationEstimate::left_multiply(arma::vec x) {
+
+  //
+  // want to compute A_tilde x where
+  //
+  // A_tilde = P_{omega tilde} (A) - P_L (Z) - P_U (Z) + Z, where Z = U d V^t
+  //
+  // see Section 3.3.1 Feasible Implementation of Hayes and Rohe (2024)
+  //
+
+  arma::vec p1 = A * x; //  P_{omega tilde} (A) x
+
+  // see p_u_zx_impl(args$u, args$d, args$v, x, getOption("Ncpus", 1L))
+  // for initial implementation
+  arma::vec p2 =  arma::zeros<arma::vec>(U.n_rows); //  P_L (Z)
+
+  // just DVt at this point
+  arma::mat W = diagmat(d) * V.t();
+
+  for (int j = 0; j < W.n_cols; j++) {
+    W.col(j) *= x(j);
+  }
+
+  // cumulative summation step
+
+  // the farthest right column should should always be all zero
+  // add a column of zeros to the right side of matrix
+  W.insert_cols(W.n_cols, 1);
+
+  // perform the cumulative summation from right to left
+  // skip the rightmost two columns, which are already fine
+  // and the leftmost column, which we will drop
+  for (int j = W.n_cols - 3; j > 0; j--) {
+    W.col(j) += W.col(j + 1);
+  }
+
+  // drop the leftmost column. now we have W_tilde in full
+  W.shed_col(0);
+
+  for (int j = 0; j < U.n_rows; j++) {
+    p2(j) = dot(U.row(j), W.col(j));
+  }
+
+  // see p_u_tilde_zx_impl(args$u, args$d, args$v, mask@i, mask@j, x)
+  // for initial implementation
+  arma::vec p3 = arma::zeros<arma::vec>(U.n_rows); // P_U (Z)
+
+  arma::sp_mat::const_iterator it     = A.begin();
+  arma::sp_mat::const_iterator it_end = A.end();
+
+  int i, j;
+  double z_ij;
+
+  for(; it != it_end; ++it) {
+
+    i = it.row(); // row index of current item
+    j = it.col(); // col index of current item
+
+    if (i >= j) {
+      z_ij = accu(U.row(i) % d % V.row(j));
+      p3(i) += x(j) * z_ij;
+    }
+  }
+
+  arma::vec p4 =  U * diagmat(d) * V.t() * x; //  Z x
+  return p1 - p2 - p3 + p4;
 }
 
-class SparseLRMatrix {
-  public:
-    SparseLRMatrix( arma::sp_mat sparse,  ) : value(v) {}
-    double square() {return value*value;}
-  private:
-    double value;
-};
+arma::vec CitationEstimate::right_multiply(arma::vec x) {
 
+  //
+  // want to compute x A_tilde where
+  //
+  // A_tilde = P_{omega tilde} (A) - P_L (Z) - P_U (Z) + Z, where Z = U d V^t
+  //
+  // we compute Atilde^T x instead
+  //
+  // see Section 3.3.1 Feasible Implementation of Hayes and Rohe (2024)
+  //
 
-class Rectangle {
-  int width, height;
-public:
-  void set_values (int,int);
-  int area() {return width*height;}
-};
+  arma::vec p1 = A.t() * x; //  P_{omega tilde} (A)^T x
 
-void Rectangle::set_values (int x, int y) {
-  width = x;
-  height = y;
+  // see p_u_ztx_impl(args$u, args$d, args$v, x, getOption("Ncpus", 1L))
+  // for initial implementation
+  arma::vec p2 =  arma::zeros<arma::vec>(V.n_rows); //  P_L (Z)^T
+
+  // just UD at this point
+  arma::mat S = U * diagmat(d);
+
+  // multiply rows by x to obtain W
+  for (int i = 0; i < S.n_rows; i++) {
+    S.row(i) *= x(i);
+  }
+
+  // cumulative summation step
+
+  S = cumsum(S); // column-wise by default
+  S.insert_rows(0, 1); // add 1 row of zeros at zeroth row index
+  S.shed_row(S.n_rows - 1);
+
+  for (int i = 0; i < V.n_rows; i++) {
+    p2(i) = dot(S.row(i), V.row(i));
+  }
+
+  // see p_u_tilde_ztx_impl(args$u, args$d, args$v, mask@i, mask@j, x)
+  // for initial implementation
+  arma::vec p3 = arma::zeros<arma::vec>(V.n_rows); // P_U (Z)
+
+  arma::sp_mat::const_iterator it     = A.begin();
+  arma::sp_mat::const_iterator it_end = A.end();
+
+  int i, j;
+  double z_ij;
+
+  for(; it != it_end; ++it) {
+
+    i = it.row(); // row index of current item
+    j = it.col(); // col index of current item
+
+    // only elements of lower triangle and the diagonal
+    if (i >= j) {
+      z_ij = accu(U.row(i) % d % V.row(j));
+      p3(j) += x(i) * z_ij;
+    }
+  }
+
+  arma::vec p4 =  V * diagmat(d) * U.t() * x; //  Z^T x
+  return p1 - p2 - p3 + p4;
 }
 
-int main () {
-  Rectangle rect;
-  rect.set_values (3,4);
-  cout << "area: " << rect.area();
-  return 0;
+
+double CitationEstimate::p_omega_z() {
+
+  // computes two of the terms in proposition 3.2
+  //
+  // p_L(Z) and P_U (Z) terms
+
+  arma::mat DVt = diagmat(d) * V.t();
+  double f_norm_sq = 0;
+
+  arma::sp_mat::const_iterator it     = A.begin();
+  arma::sp_mat::const_iterator it_end = A.end();
+
+  int i, j;
+  double z_ij;
+
+  for(; it != it_end; ++it) {
+
+    i = it.row(); // row index of current item
+    j = it.col(); // col index of current item
+
+    // only elements of lower triangle and the diagonal
+    if (i >= j) {
+      z_ij = dot(U.row(i), DVt.col(j));
+      f_norm_sq += pow(z_ij, 2);
+    }
+  }
+
+  // second add all the elements of the upper triangle
+
+  int r = U.n_cols;
+
+  // #pragma omp parallel for reduction(+: f_norm_sq)
+  for (int l = 0; l < r; l++) {
+    for (int q = 0; q < r; q++) {
+
+      arma::vec U_lq;
+      arma::rowvec V_lq, V_lq_tri;
+
+      U_lq = U.col(l) % U.col(q);
+      V_lq = DVt.row(l) % DVt.row(q);
+      V_lq_tri = sum(V_lq) - cumsum(V_lq);
+
+      f_norm_sq += dot(U_lq, V_lq_tri);
+    }
+  }
+
+  return f_norm_sq;
 }
-
-
-
-area: 12
 
 // [[Rcpp::export]]
-Rcpp::XPtr<Double> makeDouble(double x) {
-  Double* pd = new Double(x);
-  Rcpp::XPtr<Double> ptr(pd);
+Rcpp::XPtr<CitationEstimate> makeCitationEstimate(arma::sp_mat citations, arma::mat U, arma::rowvec d, arma::mat V) {
+  CitationEstimate* p = new CitationEstimate(citations, U, d, V);
+  Rcpp::XPtr<CitationEstimate> ptr(p);
   return ptr;
 }
 
 // [[Rcpp::export]]
-double squareDouble(Rcpp::XPtr<Double> x) {
-  return x.get()->square();
+arma::vec left(arma::vec x, Rcpp::XPtr<CitationEstimate> A) {
+  return A.get() -> left_multiply(x);
 }
 
-/***R
-(d2 <- makeDouble(5.4))
-squareDouble(d2)
-
-m = 100
-n = 20
-k = 5
-set.seed(111)
-A = matrix(rnorm(m * n), m)
-
-svds(A, k)
-svds(t(A), k, nu = 0, nv = 3)
-
-## Sparse matrices
-library(Matrix)
-A[sample(m * n, m * n / 2)] = 0
-Asp1 = as(A, "dgCMatrix")
-Asp2 = as(A, "dgRMatrix")
-
-svds(Asp1, k)
-svds(Asp2, k, nu = 0, nv = 0)
-
-## Function interface
-Af = function(x, args)
-{
-  as.numeric(args %*% x)
+// [[Rcpp::export]]
+arma::vec right(arma::vec x, Rcpp::XPtr<CitationEstimate> A) {
+  return A.get() -> right_multiply(x);
 }
 
-Atf = function(x, args)
-{
-  as.numeric(crossprod(args, x))
+// [[Rcpp::export]]
+double p_omega_z(Rcpp::XPtr<CitationEstimate> A) {
+  return A.get() -> p_omega_z();
 }
-
-svds(Af, k, Atrans = Atf, dim = c(m, n), args = Asp1)
-
-# s_new <- svds(
-#   Ax_citation,
-#   k = rank,
-#   Atrans = Atx_citation,
-#   dim = dim(X),
-#   args = args
-# )
-
-*/
